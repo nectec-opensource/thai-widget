62ebcdb23906c8e83629faef47c49169bea251b9
Piruin Panichphol <piruin.p@gmail.com>
2015-10-15 21:21:28 +0700

    initial commit

---
+++ domain/src/main/java/th/or/nectec/domain/thai/CitizenId.java
@@ -0,0 +1,4 @@
+package th.or.nectec.domain.thai;
+
+public class CitizenId {
+}
---
+++ domain/src/test/java/th/or/nectec/domain/thai/CitizenIdTest.java
@@ -0,0 +1,10 @@
+package th.or.nectec.domain.thai;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Blaze on 10/15/2015.
+ */
+public class CitizenIdTest {
+
+}
