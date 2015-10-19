a85cabc5efc01fd0b4130683bd3209ab6b433ebd
Piruin Panichphol <piruin.p@gmail.com>
2015-10-19 21:28:46 +0700

    Add  Apache 2.0 License

--- widget/src/main/java/th/or/nectec/android/widget/thai/CitizenIdEditText.java
+++ widget/src/main/java/th/or/nectec/android/widget/thai/CitizenIdEditText.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright © 2015 NECTEC
+ * National Electronics and Computer Technology Center, Thailand
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package th.or.nectec.android.widget.thai;
 
 import android.content.Context;
@@ -13,9 +30,6 @@ import android.widget.EditText;
 
 import th.or.nectec.domain.thai.CitizenId;
 
-/**
- * Created by blaze on 10/17/2015 AD.
- */
 public class CitizenIdEditText extends EditText implements PrettyEditText {
 
     private static final int MAX_LENGTH = 17;
