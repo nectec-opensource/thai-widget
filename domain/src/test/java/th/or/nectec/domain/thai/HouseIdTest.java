1785aa8b8902f1edd82b71e0818e80a72b4343b2
Piruin Panichphol <piruin.p@gmail.com>
2015-10-24 00:39:04 +0700

    Create test class for HouseId and except id length must not less than 11

---
+++ domain/src/test/java/th/or/nectec/domain/thai/HouseIdTest.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright © 2015 NECTEC
+ * National Electronics and Computer Technology Center, Thailand
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package th.or.nectec.domain.thai;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class HouseIdTest {
+
+    private boolean invalid = false;
+    private boolean valid = true;
+
+    @Test
+    public void validFormatId(){
+       HouseId hid = new HouseId("12345678901");
+       assertEquals("12345678901 is valid format id",valid, hid.isValidFormat());
+    }
+
+    @Test
+    public void setInvalidFormatId(){
+        HouseId hid;
+        hid = new HouseId("123456");
+        assertEquals("Id's length less than 11 must be invalid", invalid, hid.isValidFormat() );
+        hid = new HouseId("1234567890");
+        assertEquals("Id's length less than 11 must be invalid", invalid, hid.isValidFormat() );
+       
+    }
+
+    private static class HouseId {
+
+
+        public static final int LENGTH = 11;
+        private String id;
+
+        public HouseId(String id) {
+
+            this.id = id;
+        }
+
+        public  boolean isValidFormat() {
+
+            if(id.length() < LENGTH){
+                return false;
+            }
+            return true;
+        }
+    }
+}
