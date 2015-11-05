61d3d5da6f821d659bc9a6ddcb870c47367dcdf3
N. Choatravee <ch.n.17699@gmail.com>
2015-11-05 14:24:35 +0700

    สร้าง Repository สำหรับการดึงข้อมูล Region (ภูมิภาค)

---
+++ domain/src/main/java/th/or/nectec/entity/address/region/RegionChooser.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2015 NECTEC
+ * National Electronics and Computer Technology Center, Thailand
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package th.or.nectec.entity.address.region;
+
+import java.util.List;
+
+/**
+ * Created by N. Choatravee on 5/11/2558.
+ */
+public class RegionChooser {
+    private final RegionRepository regionRepository;
+    private final RegionPresenter regionPresenter;
+
+    public RegionChooser(RegionRepository regionRepository, RegionPresenter regionPresenter) {
+        this.regionRepository = regionRepository;
+        this.regionPresenter = regionPresenter;
+    }
+
+    public void showAllRegionList() {
+        List<String> allRegionList = regionRepository.getAllRegions();
+        if (allRegionList != null) {
+            regionPresenter.showRegionList(allRegionList);
+        } else {
+            regionPresenter.showNotFoundRegion();
+        }
+    }
+}
