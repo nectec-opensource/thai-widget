5f42e66e467de95784e66849f2c6c9d9708bd0b8
N. Choatravee <ch.n.17699@gmail.com>
2015-11-06 10:14:17 +0700

    เพิ่มการตรวจสอบ string ที่ null และ empty ใน TextUtils

--- domain/src/main/java/th/or/nectec/util/TextUtils.java
+++ domain/src/main/java/th/or/nectec/util/TextUtils.java
@@ -1,12 +1,12 @@
 /*
- * Copyright © 2015 NECTEC
+ * Copyright 2015 NECTEC
  * National Electronics and Computer Technology Center, Thailand
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *    http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -29,6 +29,10 @@ public class TextUtils {
 
     public static boolean isRepeatPatternNumber(String text) {
         return text.matches("\\b(\\d+)\\1+\\b");
+    }
+
+    public static boolean isEmpty(String text) {
+        return text != null && !text.isEmpty();
     }
 
 
