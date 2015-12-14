36346248116a3e05a47934de4f998ab4cc30ff3e
Piruin Panichphol <piruin.p@gmail.com>
2015-12-14 21:23:18 +0700

    แยกโค้ดส่วนการแปลงไฟล์ JSON เป็น Object ออกจาก InMemoryJsonRepository

---
+++ widget/src/main/java/th/or/nectec/android/widget/thai/address/JsonAdapter.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2015 NECTEC
+ *   National Electronics and Computer Technology Center, Thailand
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package th.or.nectec.android.widget.thai.address;
+
+import android.content.Context;
+import com.google.gson.Gson;
+import com.google.gson.stream.JsonReader;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class JsonAdapter {
+
+    public static <T> ArrayList<T> parse(Context context, String jsonFileName, Class<T> tClass) {
+
+        ArrayList<T> allProvince = new ArrayList<>();
+        Gson gson = new Gson();
+        try {
+            InputStream inputStream = context.getAssets().open(jsonFileName);
+            JsonReader reader = new JsonReader(new InputStreamReader(inputStream, "UTF-8"));
+            reader.beginArray();
+            while (reader.hasNext()) {
+                T message = gson.fromJson(reader, tClass);
+                allProvince.add(message);
+            }
+            reader.endArray();
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return allProvince;
+    }
+}
