57b046b0b20841d07c5bea8662c495891e8b2419
Piruin Panichphol <piruin.p@gmail.com>
2015-12-14 21:49:16 +0700

    Rename typo getMaxLength method

--- widget/src/main/java/th/or/nectec/android/widget/thai/HouseIdHandler.java
+++ widget/src/main/java/th/or/nectec/android/widget/thai/HouseIdHandler.java
@@ -1,20 +1,18 @@
 /*
- * Copyright © 2015 NECTEC
- * National Electronics and Computer Technology Center, Thailand
+ * Copyright (c) 2015 NECTEC
+ *   National Electronics and Computer Technology Center, Thailand
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *    http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
- *
  */
 
 package th.or.nectec.android.widget.thai;
@@ -33,7 +31,7 @@ public class HouseIdHandler extends IdentityEditTextHandler {
     }
 
     @Override
-    protected int getMaxLenght() {
+    protected int getMaxLength() {
         return MAX_LENGTH;
     }
 
